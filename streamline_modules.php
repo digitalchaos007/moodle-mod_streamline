79e33c915bb0f8843605ef6f6dbdd24cbae740a9
Akshay Saini <u5332580@anu.edu.au>
2015-09-13 21:51:55 +1000

    Added alt tags to Webinar Buttons

--- streamline_modules.php
+++ streamline_modules.php
@@ -79,7 +79,7 @@
 		</div>
 		<div  class="unit-25" id="rightContainer">
 			<div id="webinar_buttons">
-				<div id="std_button" class="fullscreen_button">
+				<div id="std_button" class="fullscreen_button" title="Full screen">
 					<img src="./images/fullscreen_button.png" style='width: 100%; object-fit: contain; visibility:hidden' />
 				</div>
 				<div id="std_button" class="quiz_button" class="dropdown" id="dropdownMenu1" title="Quiz">
@@ -89,7 +89,7 @@
 				  <ul id="quiz_menu" class="dropdown-menu" aria-labelledby="dropdownMenu1">
 				  </ul>
 				</div>
-				<div id="std_button" class="leave_button">
+				<div id="std_button" class="leave_button" title="Exit">
 					<img src="./images/fullscreen_button.png" style='width: 100%; object-fit: contain; visibility:hidden' />
 				</div>
 			</div>
