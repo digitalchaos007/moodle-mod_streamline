063286b15662e777ff5e58878a058cf797c23572
Cookiearecoolcookies <venkata_maruti_vadrevu@hotmail.com>
2015-10-04 19:16:47 +1100

    Added the quiz database

--- Quiz/quiz_submit.php
+++ Quiz/quiz_submit.php
@@ -1,5 +1,8 @@
 <?php
 
+
+	global $DB;
+
 	$qid = $_POST['qid'];
 	$sid = $_POST['sid'];
 	$cid = $_POST['cid'];
@@ -15,8 +18,28 @@
 		fwrite($testFile, $answers[0].'\n');	
 	}
 	
-	fclose($testFile);
+
+        $N_record1 = new stdClass();
+        $N_record1->quizid  = (int) $qid;
+        $N_record1->streamlineid = (int) $sid;
+        $N_record1->courseid = (int) $cid;
+        $N_record1->userid  = (int) $stuid;
+        $N_record1->answers = (string) $answers;
+        $arr = array($N_record1);
+
+        $table = 'streamline_quiz';
+    try {
+        $DB->insert_records($table, $arr);
+				$error = 'Always throw this error';
+				 throw new Exception($error);
+
+    } catch (Exception $e) {
+				fwrite($testFile, 'error '.$e.'\n');
+    }
  
+    fwrite($testFile, 'completed 5 '.'\n');
+ 
+    fclose($testFile);
 
 
 ?>
