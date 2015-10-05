84735e1485374ddde5a3365263c2dd0b31c81c11
matt <matthew.wmitchell101@gmail.com>
2015-10-05 19:20:02 +1100

    Fixed quiz_submit.php to submit answers to table.

--- Quiz/quiz_submit.php
+++ Quiz/quiz_submit.php
@@ -9,23 +9,23 @@ require_once(dirname(dirname(dirname(dirname(__FILE__)))).'/config.php');
 	$cid = $_POST['cid'];
 	$stuid = $_POST['stuid'];
 	$answers = $_POST['answers'];
-
+	$answer_string = "";
 	$testFile = fopen("quiz.txt", "w") or die("Unable to open file!");
 	fwrite($testFile, $qid.'\n');
 	fwrite($testFile, $sid.'\n');
 	fwrite($testFile, $cid.'\n');
 	fwrite($testFile, $stuid.'\n');
-	for($i = 0; $i < sizeof($answers);$i++) {
-		fwrite($testFile, $answers[0].'\n');	
+	for((int) $i = 0; $i < sizeof($answers); $i++) {
+		fwrite($testFile, $answers[1].'\n answers ' . $i);	
+		$answer_string = $answer_string . (string) $answers[$i] . ';';
 	}
-	
 
         $N_record1 = new stdClass();
         $N_record1->quizid  = (int) $qid;
         $N_record1->streamlineid = (int) $sid;
         $N_record1->courseid = (int) $cid;
         $N_record1->userid  = (int) $stuid;
-        $N_record1->answers = (string) $answers;
+        $N_record1->answers = (string) $answer_string;
         $arr = array($N_record1);
 
         $table = 'streamline_quiz';
